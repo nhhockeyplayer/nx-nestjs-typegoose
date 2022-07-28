fa60458402df1f628735c6cfd2be0b5988225e98
nhhockeylayer <meanstack@hotmail.com>
2022-07-28 04:43:49 -0400

    chore: initial setup

---
+++ packages/nestjs-typegoose/README.md
@@ -0,0 +1,9 @@
+# nestjs-typegoose
+
+This library was generated with [Nx](https://nx.dev).
+
+
+## Running unit tests
+
+Run `nx test nestjs-typegoose` to execute the unit tests.
+
---
+++ packages/nestjs-typegoose/src/index.ts
@@ -0,0 +1,10 @@
+export * from './lib/nestjs-typegoose.module';
+export * from './lib/typegoose.constants';
+export * from './lib/typegoose.decorators';
+export * from './lib/typegoose-class.interface';
+export * from './lib/typegoose-core.module';
+export * from './lib/typegoose.module';
+export * from './lib/typegoose.providers';
+export * from './lib/typegoose.utils';
+export * from './lib/typegoose-options.interface';
+
